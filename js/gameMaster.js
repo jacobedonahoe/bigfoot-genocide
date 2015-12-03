7e0a89bca7700acafdf010eab572f7e5ffabdb4f
Tyler Kerkhof <kerkhof.tyler@gmail.com>
2015-12-03 01:52:01 -0600

    Updated player sprite and started adding code for sprite sheet preloading

--- js/gameMaster.js
+++ js/gameMaster.js
@@ -3,8 +3,10 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload, create:
 
 
 function preload() {
-    
-}
+    game.load.spritesheet('player', 'assets/sprites/characters/player.png', 32, 32);
+//    game.load.spritesheet('bigfoot', 'assets/sprites/characters/bigfoot1.png', 32, 64);
+//    game.load.spritesheet('nazi', 'assets/sprites/characters/nazi.png', 32, 32);
+//}
 
 
 
