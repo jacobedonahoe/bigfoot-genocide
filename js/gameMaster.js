ec5faffa58a1aa33ca09729aa2f8fab35fe93109
Tyler Kerkhof <kerkhof.tyler@gmail.com>
2015-12-07 21:45:55 -0600

    Bigfoot now chases player! Logic for moving Bigfoot around when he's first created is broken, so not using for now.

--- js/gameMaster.js
+++ js/gameMaster.js
@@ -3,14 +3,18 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {preload: preload,
                                                        update: update});
 
 var bigfootXPos = 500,
-    bigfootYPos = 90;
+    bigfootYPos = 90,
+    bigfootTween1,
+    bigfootTween2,
+    bigfootTween3,
+    bigfootTween4,
+    bigfootDirection;
 
 
 
 function preload() {
     game.load.spritesheet('player', 'assets/sprites/characters/player.png', 32, 32);
     game.load.spritesheet('bigfoot', 'assets/sprites/characters/bigfoot1.png', 85, 120);
-//    game.load.spritesheet('nazi', 'assets/sprites/characters/nazi.png', 32, 32);
 }
 
 
@@ -73,95 +77,92 @@ function setupBigfoot() {
     
     bigfoot.animations.play('standBottomLeft');
     
-    addBigfootDocileBehavior();
+//    addBigfootDocileBehavior();
 }
 
 
 
 function addBigfootDocileBehavior() {    
-    var tween1 = game.add.tween(bigfoot).to({x: bigfootXPos - 100, y: bigfootYPos + 100},
+    bigfootTween1 = game.add.tween(bigfoot).to({x: bigfootXPos - 100, y: bigfootYPos + 100},
                                             1200,
                                             Phaser.Easing.Linear.None,
                                             false,
                                             3000);
-    tween1.onStart.add(function() {
+    var bigfootTween2 = game.add.tween(bigfoot).to({x: bigfootXPos + 100, y: bigfootYPos + 100},
+                                            1200,
+                                            Phaser.Easing.Linear.None,
+                                            false,
+                                            3000);
+    var bigfootTween3 = game.add.tween(bigfoot).to({x: bigfootXPos + 100, y: bigfootYPos - 100},
+                                            1200,
+                                            Phaser.Easing.Linear.None,
+                                            false,
+                                            3000);
+    var bigfootTween4 = game.add.tween(bigfoot).to({x: bigfootXPos - 100, y: bigfootYPos - 100},
+                                            1200,
+                                            Phaser.Easing.Linear.None,
+                                            false,
+                                            3000);
+    
+    bigfootTween1.onStart.add(function() {
         bigfoot.animations.play('walkBottomLeft');
     }, this);
-    
-    tween1.onComplete.add(function() {
-        bigfoot.animations.play('standBottomRight');
-        bigfootXPos = bigfoot.body.x;
-        bigfootYPos = bigfoot.body.y;
-        tween2.start();
-    }, this);
-    
-    
-    
-    
-    var tween2 = game.add.tween(bigfoot).to({x: bigfootXPos + 100, y: bigfootYPos + 100},
-                                            1200,
-                                            Phaser.Easing.Linear.None,
-                                            false,
-                                            3000);
-    tween2.onStart.add(function() {
+    bigfootTween2.onStart.add(function() {
         bigfoot.animations.play('walkBottomRight');
     }, this);
-    
-    tween2.onComplete.add(function() {
-        bigfoot.animations.play('standTopRight');
-        bigfootXPos = bigfoot.body.x;
-        bigfootYPos = bigfoot.body.y;
-        tween3.start();
-    }, this);
-    
-    
-    
-    
-    var tween3 = game.add.tween(bigfoot).to({x: bigfootXPos + 100, y: bigfootYPos - 100},
-                                            1200,
-                                            Phaser.Easing.Linear.None,
-                                            false,
-                                            3000);
-    tween3.onStart.add(function() {
+    bigfootTween3.onStart.add(function() {
         bigfoot.animations.play('walkTopRight');
     }, this);
-    
-    tween3.onComplete.add(function() {
-        bigfoot.animations.play('standTopLeft');
-        bigfootXPos = bigfoot.body.x;
-        bigfootYPos = bigfoot.body.y;
-        tween4.start();
-    }, this);
-    
-    
-    
-    
-    var tween4 = game.add.tween(bigfoot).to({x: bigfootXPos - 100, y: bigfootYPos - 100},
-                                            1200,
-                                            Phaser.Easing.Linear.None,
-                                            false,
-                                            3000);
-    tween4.onStart.add(function() {
+    bigfootTween4.onStart.add(function() {
         bigfoot.animations.play('walkTopLeft');
     }, this);
     
-    tween4.onComplete.add(function() {
+    bigfootTween1.onComplete.add(function() {
+        bigfoot.animations.play('standBottomRight');
+        bigfootXPos = bigfoot.body.x;
+        bigfootYPos = bigfoot.body.y;
+        bigfootTween2.start();
+    }, this);
+    bigfootTween2.onComplete.add(function() {
+        bigfoot.animations.play('standTopRight');
+        bigfootXPos = bigfoot.body.x;
+        bigfootYPos = bigfoot.body.y;
+        bigfootTween3.start();
+    }, this);
+    bigfootTween3.onComplete.add(function() {
+        bigfoot.animations.play('standTopLeft');
+        bigfootXPos = bigfoot.body.x;
+        bigfootYPos = bigfoot.body.y;
+        bigfootTween4.start();
+    }, this);    
+    bigfootTween4.onComplete.add(function() {
         bigfoot.animations.play('standBottomLeft');
         bigfootXPos = bigfoot.body.x;
         bigfootYPos = bigfoot.body.y;
-        tween1.start();
+        bigfootTween1.start();
     }, this);
     
-    
-    
-    
-    
-    tween1.start();
+    bigfootTween1.start();
 }
 
 
 
 function update() {
+    checkMovePlayer();
+    
+    if (game.physics.arcade.distanceBetween(bigfoot, player) < 200) {
+        game.tweens.removeFrom(bigfoot);
+        game.physics.arcade.moveToObject(bigfoot, player);
+        
+        setBigfootsDirection();        
+    } else {
+        stopBigfoot();
+    }
+}
+
+
+
+function checkMovePlayer() {
     player.body.velocity.x = 0;
     player.body.velocity.y = 0;
     
@@ -179,7 +180,64 @@ function update() {
         player.animations.play('walkRight');
     } else {
         player.animations.stop();
-        //TODO: Player direction logic
     }
+}
+
+
+
+function setBigfootsDirection() {
+    var currentAngle = (bigfoot.body.angle * 180) / Math.PI;
     
+    if (valueIsBetween(currentAngle, 90, 180)) {
+        if (bigfoot.animations.currentAnim != bigfoot.animations.getAnimation("walkBottomLeft")) {
+            bigfoot.animations.play("walkBottomLeft");
+            bigfootDirection = "bottomLeft";
+        }
+    } else if (valueIsBetween(currentAngle, 0, 90)) {
+        if (bigfoot.animations.currentAnim != bigfoot.animations.getAnimation("walkBottomRight")) {
+            bigfoot.animations.play("walkBottomRight");
+            bigfootDirection = "bottomRight";
+        }
+    } else if (valueIsBetween(currentAngle, -90, 0)) {
+        if (bigfoot.animations.currentAnim != bigfoot.animations.getAnimation("walkTopRight")) {
+            bigfoot.animations.play("walkTopRight");
+            bigfootDirection = "topRight";
+        }
+    } else if (valueIsBetween(currentAngle, -180, -90)) {
+        if (bigfoot.animations.currentAnim != bigfoot.animations.getAnimation("walkTopLeft")) {
+            bigfoot.animations.play("walkTopLeft");
+            bigfootDirection = "topLeft";
+        }
+    } 
+}
+
+
+
+function stopBigfoot() {
+    bigfoot.body.velocity.x = 0;
+    bigfoot.body.velocity.y = 0;
+
+    switch (bigfootDirection) {
+        case "bottomLeft":
+            bigfoot.animations.play("standBottomLeft");
+            break;
+        case "bottomRight":
+            bigfoot.animations.play("standBottomRight");
+            break;
+        case "topLeft":
+            bigfoot.animations.play("standTopLeft");
+            break;
+        case "topRight":
+            bigfoot.animations.play("standTopRight");
+            break;
+        default:
+            bigfoot.animations.play("standBottomLeft");
+            break;
+    }
+}
+
+
+
+function valueIsBetween(toCompareVal, smallVal, bigVal) {
+    return toCompareVal > smallVal && toCompareVal < bigVal;
 }
